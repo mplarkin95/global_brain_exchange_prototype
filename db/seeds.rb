f81fd9638517b9c17377b59e5617f1b5c9981771
Matt <lilpigboy@Thunderdome2.0>
2017-04-29 15:22:13 -0400

    added seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+students = Student.create([{name: "Matt", bio:"Matt's  bio", gpa: 4.0, school: 'Clark'}, {name: "Sam", bio: "Sam's Bio",gpa: 2.0, school: "Amherst"}])
+
+recruiters = Recruiter.create([{name: "Alex",bio: "alex's bio"},{name: "Arthur", bio:"arthur's bio"}])
+
+users = User.create([
+	{email: "matt@gbx.com",password: "qweqwe", password_confirmation: "qweqwe", meta_id: 1, meta_type: "Student"},
+	{email: "sam@gbx.com", password: "qweqwe", password_confirmation: "qweqwe", meta_id: 2, meta_type: "Student"},
+	{email: "alex@gbx.com", password: "qweqwe", password_confirmation: "qweqwe", meta_id: 1, meta_type: "Recruiter"},
+	{email: "arthur@gbx.com", password: "qweqwe", password_confirmation: "qweqwe", meta_id: 2, meta_type: "Recruiter"}])
